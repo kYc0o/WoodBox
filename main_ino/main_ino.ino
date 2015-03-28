8dd00da0b0bf9b77d6f8c2109b7c0380dcd5b950
kYc0o <fco.ja.ac@gmail.com>
2015-03-28 10:08:28 +0100

    add some comments

--- main_ino/main_ino.ino
+++ main_ino/main_ino.ino
@@ -1,36 +1,41 @@
+#include <ChainableLED.h>
+
 #include "DHT.h"
 #include"AirQuality.h"
 #include"Arduino.h"
 
 // DHT
-#define DHTPIN 8     // what pin we're connected to
-#define DHTTYPE DHT11   // DHT 11 
+#define DHTPIN 8      // Digital pin for DHT sensor
+#define DHTTYPE DHT11  // DHT 11 sensor type
+#define LUX_PIN A3    // Analog pin for luminosity sensor
+#define AIR_PIN A0    // Analog pin for air quality sensor
+
 // Initialize DHT sensor for normal 16mhz Arduino
 DHT dht(DHTPIN, DHTTYPE);
 
-#define LUX_PIN A3
-#define AIR_PIN A0
-
-
+// Air quality sensor object
 AirQuality airqualitysensor;
 
+// Global variables for sensors
 int qualiteAir =-1;
 float temperature = 0;
 float humidite = 0;
 int luminosite = 0;
+
+// Data count
 int count = 0;
 
+// Incoming data for actions
 int incomingByte;
 
 
 void setup() 
 {
-  Serial.begin(9600);  
-  
+  // Serial output at 9600 baud
+  Serial.begin(9600);
+  // Initialize dht sensor
   dht.begin();
-  airqualitysensor.init(14);
-
-  
+  airqualitysensor.init(14);  
 }
 
 
@@ -46,14 +51,16 @@ void loop()
   luminosite = analogRead(LUX_PIN);
   qualiteAir = airqualitysensor.slope();      
 
-   /*if (Serial.available() > 0) {
+   if (Serial.available() > 0) {
                 // read the incoming byte:
                 incomingByte = Serial.read();
+
+
 
                 // say what you got:
                 Serial.print("I received: ");
                 Serial.println(incomingByte, DEC);
-        }*/
+        }
    printInfo(temperature, humidite, luminosite, qualiteAir);
 
    count ++;
